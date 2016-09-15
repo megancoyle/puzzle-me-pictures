67f641e657bd5a8a24cb0bf8356ac10ad1022153
Megan Coyle <meg@mcoyle.com>
2016-09-15 10:52:12 -0400

    Update config

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -2,6 +2,6 @@ class Puzzle < ApplicationRecord
   mount_uploader :image, PuzzleUploader
   validates :title, presence: true,
                       length: { minimum: 5 }
-  validates :image, file_size: { less_than_or_equal_to: 100.kilobytes },
+  validates :image, file_size: { less_than_or_equal_to: 200.kilobytes },
             file_content_type: { allow: ['image/jpeg', 'image/png'] }
 end
